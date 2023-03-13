10c01c76ee350d0ff7a071c7a088447cbd58fc5c
jdarwood007 <unmonitored+github@sleepycode.com>
2023-03-13 15:47:20 -0700

    Add Spanish languages Update install to handle upgrades

---
+++ languages/DevTools.spanish_latin.php
@@ -0,0 +1,22 @@
+<?php
+/* Spanish translation v1.0 */
+$txt['devtools_menu'] = 'Herramientas de desarrollo';
+
+/* Packages stuff */
+$txt['devtools_packages_uninstall'] = 'Desinstalar ganchos';
+$txt['devtools_packages_reinstall'] = '(Re)instalar ganchos';
+$txt['devtools_packages_syncin'] = 'Sincronizar archivos con el paquete';
+$txt['devtools_packages_syncout'] = 'Sincronizar archivos con SMF';
+$txt['devtools_hook_reinstall_fail'] = 'Error al reinstalar ganchos';
+
+$txt['devtools_success_reinstall'] = 'Ganchos (re)instalados con éxito';
+$txt['devtools_success_uninstall'] = 'Ganchos desinstalados con éxito';
+$txt['devtools_success_syncin'] = 'Archivos sincronizados correctamente con paquetes';
+$txt['devtools_success_syncout'] = 'Archivos sincronizados con éxito con SMF';
+
+$txt['devtools_instance'] = 'Instancia';
+
+$txt['devtools_success_toggle'] = 'Alternado correctamente el gancho';
+$txt['devtools_success_addhook'] = 'Gancho añadido con éxito';
+$txt['devtools_success_modifyhook'] = 'Gancho modificado con éxito';
+$txt['devtools_success_deletehook'] = 'Gancho eliminado con éxito';
