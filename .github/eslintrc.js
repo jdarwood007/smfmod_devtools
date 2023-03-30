f5dfe2702bc1b0c0beb81d66bc0d6eb5f37a263c
jdarwood007 <unmonitored+github@sleepycode.com>
2023-03-29 18:30:27 -0700

    Try to fix some eslint issues

--- .github/eslintrc.js
+++ .github/eslintrc.js
@@ -30,5 +30,11 @@ module.exports = {
 				'args' : 'none'
 			}
 		]
+	},
+	'globals': {
+		'var2': 'smf_scripturl',
+		'var2': 'txt_devtools_menu',
+		'var2': 'smc_PopupMenu',
+		'var2': 'allow_xhjr_credentials'
 	}
 };
