dbd6d696ca28d16d07a17042f4fe49c004c53816
Bugo <cheetah@bk.ru>
2023-03-31 16:16:03 +0500

    Update Russian translation

--- languages/DevTools.russian.php
+++ languages/DevTools.russian.php
@@ -1,5 +1,6 @@
 <?php
 $txt['devtools_menu'] = 'Инструменты разработчика';
+$txt['files_title_list'] = 'Архивы';
 
 /* Packages stuff */
 $txt['devtools_packages_uninstall'] = 'Удалить хуки';
@@ -13,11 +14,13 @@ $txt['devtools_success_uninstall'] = 'Хуки успешно удалены';
 $txt['devtools_success_syncin'] = 'Файлы успешно синхронизированы с пакетом';
 $txt['devtools_success_syncout'] = 'Файлы успешно синхронизированы с SMF';
 
-
 $txt['devtools_instance'] = 'Экземпляр объекта';
-
 
 $txt['devtools_success_toggle'] = 'Хук успешно переключен';
 $txt['devtools_success_addhook'] = 'Хук успешно добавлен';
 $txt['devtools_success_modifyhook'] = 'Хук успешно изменён';
 $txt['devtools_success_deletehook'] = 'Хук успешно удалён';
+
+$txt['devtools_error_archive_generation'] = 'Невозможно сгенерировать архив';
+$txt['devtools_extension_zip'] = 'ZIP';
+$txt['devtools_extension_tgz'] = 'TGZ';
